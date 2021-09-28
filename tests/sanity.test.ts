15a16fc83afe895813ceb5eb3271ae90851307db
Aniruddha Adhikary <aniruddha@adhikary.net>
2021-09-29 07:44:57 +0800

    tests: add jest test runner

---
+++ tests/sanity.test.ts
@@ -0,0 +1,7 @@
+describe('Sanity Test', () => {
+    it('should run', () => {
+        const number1: number = 1;
+        const number2: number = 1;
+        expect(number1 + number2).toBe(2)
+    })
+})
